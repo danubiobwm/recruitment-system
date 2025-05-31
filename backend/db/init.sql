0d35d002d41b26d41a92564a1ed0e8683cf349e9
José Danubio de Araujo <danubio.bwm@gmail.com>
2025-05-31 19:59:11 -0300

    remoção projeto

--- backend/db/init.sql
+++
@@ -1,5 +0,0 @@
--- Criação de tabelas adicionais (opcional)
--- Estas tabelas serão criadas automaticamente pelo GORM, mas você pode adicionar configurações específicas aqui
-
--- Extensões úteis
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
